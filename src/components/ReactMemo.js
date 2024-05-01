07b8821b9cd4e3726d4c1559eb4cc0e43daebd5a
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-01 16:36:46 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,44 +1,13 @@
-import React, { useState } from "react";
+// ReactMemo.js
+import React from "react";
 
-const ReactMemo = () => {
-  
-  const [customTask, setCustomTask] = useState(''); 
-  const [skills, setaSkills] = useState([]);
-   const handleCustomTaskChange = (e) => {
-    setCustomTask(e.target.value);
-    console.log('check2');
-  }; 
-
-    const handleCustomTaskSubmit = () => {
-    if (customTask.length > 5) {
-      setaSkills([...skills, customTask]);
-      setCustomTask('');
-      console.log('check3');
-    } else {
-      alert('Task must be more than 5 characters long.');
-    }
-  };
+const ReactMemo = ({ counter }) => {
   return (
     <div>
-      <h2>React.memo</h2>
-      <input
-        id="skill-input"
-        type="text"
-        value={customTask}
-        onChange={handleCustomTaskChange}
-        placeholder="Enter custom task"
-      />
-      <button onClick={handleCustomTaskSubmit} id="skill-btn">
-        Add Skill
-      </button>
-
-      <div id="item-jumbotron">
-        {skills.map((task, index) => (
-          <div key={index}>{task}</div>
-        ))}
-      </div>
+      <h2>React.memo Demo</h2>
+      <p>Counter value: {counter}</p>
     </div>
   );
 };
 
-export default ReactMemo;
+export default React.memo(ReactMemo);
