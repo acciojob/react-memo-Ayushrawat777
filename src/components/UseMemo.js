6412117d00c68110f312ac7539643fb20fb1762c
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-01 16:37:18 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,58 +1,22 @@
-import React, { useMemo, useState } from 'react'
+// UseMemo.js
+import React, { useMemo } from "react";
 
 const UseMemo = () => {
-  const [tasks, setTasks] = useState([]);
-  const [counter, setCounter] = useState(0);
-
-  const handleAddTodo = () => {
-    setTasks([...tasks, "New todo"]);
-    console.log('check1');
-  };
-
-  const handleRemoveTask = (index) => {
-   const data= tasks.filter((val,id)=>{
-     console.log('check4');
-      return id !== index;
-    })
-    setTasks(data)
-  };
-
-   const handleIncrement = () => {
-    setCounter(counter + 1);
-    console.log('check4');
-  };
-
+  // Memoized value for expensive calculation
+  const memoizedValue = useMemo(() => {
+    let result = 0;
+    for (let i = 0; i <= 1000000000; i++) {
+      result = i;
+    }
+    return result;
+  }, []);
+  // /1000000000
   return (
-    <div>
-         <h2>React.useMemo</h2>
-      <h2>My todos</h2>
-      <p>New Todo</p>
-      <button id="add-todo-btn" onClick={handleAddTodo}>
-        Add todo
-      </button>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button
-              id={`todo-${index}`}
-              onClick={() => handleRemoveTask(index)}
-            >
-              New Todo
-            </button>
-          </li>
-        ))}
-      </ul>
-      <hr />
-      <p>Count: {counter}
-      <button id="increment-btn" onClick={handleIncrement}>+</button>
-      </p>
-      <h2>Expensive Calculation</h2>
-      <p>1000000000</p>
-      <hr />
-<hr />
+    <div id="add-todo-btn">
+      <h2>useMemo Demo</h2>
+      <p id="calc">{memoizedValue}</p>
     </div>
-  )
-}
+  );
+};
 
-export default UseMemo
+export default UseMemo;
