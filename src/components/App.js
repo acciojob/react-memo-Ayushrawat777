cc27b6c0cd6545cef3e9b3419314ae25a4969a6e
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-01 14:26:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,17 @@
-import React,{useState} from 'react'
-import Todos from './Todos'
-import Memo from './Memo'
-import Count from './Count'
-import { useEffect } from 'react'
 
-let App = () =>{
-    useEffect(()=>{
 
-    },[count,memo,todos])
-    let [todos,Settodos]=useState(['New Todo'])
-    let [memo,Setmemo]=useState(['HTMLL',"CSSS3"])
-    let [count,Setcount]=useState(1000000000)
-    return(
-        <div id='main'>
-        <h1>React.useMEMo</h1>
-        <Todos set={Settodos} arr={todos} />
-        <Count set={Setcount} number={count}/>
-        <h1>Expensive Calculation</h1>
-        {/* <div id='calc'>1000000000</div> */}
-        <Memo set={Setmemo} arr={memo} />
-        </div>
-    )
-   
-        
-}
+import React from "react";
+import UseMemo from './UseMemo';
+import ReactMemo from './ReactMemo'; 
 
-export default App
+const App = () => {
+
+  return (
+    <div id="main">
+      <UseMemo />
+      <ReactMemo/> 
+    </div>
+  );
+};
+
+export default App;
