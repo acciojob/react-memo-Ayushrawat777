d72dc88b7e85f93bf07f13d2f2004b2e6e0a0ed5
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-02 17:13:29 +0530

    Create ReactMemo.js

---
+++ src/components/ReactMemo.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { memo } from "react";
+
+const ReactMemo = ({skills}) => {
+  console.log("child render");
+  return (
+    <div>
+      <ul>
+        {skills.map((val,index)=>{
+          return <li key={index}>{val}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default ReactMemo
