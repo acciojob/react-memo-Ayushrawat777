6bcf295287518fd20a5b35a0aa85ce2586bd3cb3
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-03 01:33:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,30 @@ import React, { useState } from "react";
 import UseMemo from "./UseMemo";
 import ReactMemo from "./ReactMemo";
 const App = () => {
-    const [skill,setSkill]=useState('')
-    const [skills,setSkills]=useState(['HTML','CSS','JavaScript','React','sdadsd'])
-    const addSkills = () => {
-        if (skill.length>5) {
-            setSkills((t) => [...t, skill]);
-        } else {
-            alert("Task need to be more then 5 character")
-        }
-    };
+  const [skill, setSkill] = useState("");
+  const [skills, setSkills] = useState([
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "sdadsd",
+  ]);
+  const addSkills = () => {
+    if (skill.length > 5) {
+      setSkills((t) => [...t, skill]);
+    } else {
+      alert("Task need to be more then 5 character");
+    }
+  };
   return (
     <div>
-     <UseMemo /> 
-     <h2>React.memo</h2>
-      <input type="text" onChange={(e)=>setSkill(e.target.value)}/>
+      <UseMemo />
+      <h2>React.memo</h2>
+      <input type="text" onChange={(e) => setSkill(e.target.value)} />
       <button onClick={addSkills}>Add Skill</button>
-      <ReactMemo skills={skills}/>
+      <ReactMemo skills={skills} />
     </div>
   );
 };
 
 export default App;
- 
