ab53cc7171d2fae13bf8d843602454bb15e9ca9a
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-03 01:33:52 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -3,23 +3,25 @@ import { useState, useMemo } from "react";
 const UseMemo = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
-  
+
   const increment = () => {
-    setCount((c) => c + 1);
+    setCount(count + 1);
   };
   const addTodo = () => {
     setTodos((t) => [...t, "New Todo"]);
   };
-  
+
   const expensiveCalculation = (num) => {
     console.log("Calculating...");
     for (let i = 0; i < 1000000000; i++) {
-      num += 1;
+      num = num + 1;
     }
     return num;
   };
-  
-  const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+  const calculation = useMemo(() => {
+    return expensiveCalculation(count);
+  }, [count]);
   return (
     <div>
       <div>
@@ -43,5 +45,3 @@ const UseMemo = () => {
   );
 };
 export default UseMemo;
-
-
