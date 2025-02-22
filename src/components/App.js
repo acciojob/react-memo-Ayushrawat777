468577c76092e0fa9b3baf3b72de43a6c8b80d13
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-22 16:20:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,60 @@
-import React, { useState } from "react";
-import UseMemo from "./UseMemo";
+import React, { useState, useMemo } from "react";
 import ReactMemo from "./ReactMemo";
+import UseMemo from "./UseMemo";
+
 const App = () => {
-  const [skill, setSkill] = useState("");
-  const [skills, setSkills] = useState([
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "sdadsd",
-  ]);
-  const addSkills = () => {
-    if (skill.length > 5) {
-      setSkills((t) => [...t, skill]);
+  const [todo, setTodo] = useState([]);
+  const [value, setValue] = useState("");
+
+  const addData = (value) => {
+    if (value.length > 5) {
+      setTodo([...todo, value]);
+      setValue("");
     } else {
-      alert("Task need to be more then 5 character");
+      alert("Task must be more than 5 characters!");
     }
   };
+
   return (
     <div>
-      <UseMemo />
-      <h2>React.memo</h2>
-      <input type="text" onChange={(e) => setSkill(e.target.value)} />
-      <button onClick={addSkills}>Add Skill</button>
-      <ReactMemo skills={skills} />
+      <h3>React.useMemo</h3>
+
+      <h3>My Todos</h3>
+      <div
+        style={{
+          borderBottom: "2px solid grey", paddingBottom:"10px",
+        }}
+      >
+        <p>New Todo</p>
+        <button onClick={() => addData("New Todo")}>Add Todo</button>
+      </div>
+
+      <div
+        style={{
+          borderBottom: "2px solid grey",
+        }}
+      >
+        <UseMemo />
+      </div>
+
+      <div
+        style={{
+          marginTop:"7px",
+          borderTop: "2px solid grey",
+        }}
+      >
+        <h3>React.memo</h3>
+        <input
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          placeholder="Enter a task..."
+        />
+        <button onClick={() => addData(value)}>Add Task</button>
+      </div>
+
+      <ul>
+        <ReactMemo todo={todo} />
+      </ul>
     </div>
   );
 };
